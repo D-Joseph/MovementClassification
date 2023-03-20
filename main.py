a81359f80d12d0865154248c0d23198ba0017c54
D-Joseph <danielrjoseph8@gmail.com>
2023-03-20 18:04:29 -0400

    Added my walking/jumping

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import h5py as h5
+
+with h5.File('./hdf5_data.h5', 'w') as hdf:
+    char = hdf.create_group('/Char')
+    char.create_dataset('walkingHand', data=pd.read_csv('CharWalkingHand.csv'), compression='gzip', compression_opts=7)
+    char.create_dataset('walkingPant', data=pd.read_csv('CharWalkingFrontPocket.csv'), compression='gzip', compression_opts=7)
+    char.create_dataset('walkingJacket', data=pd.read_csv('CharWalkingJacketPocket.csv'), compression='gzip', compression_opts=7)
+    char.create_dataset('jumpingPant', data=pd.read_csv('CharJumpingFrontPocket.csv'), compression='gzip', compression_opts=7)
+    char.create_dataset('jumpingHand', data=pd.read_csv('CharJumpingHand.csv'), compression='gzip', compression_opts=7)
+
+    nile = hdf.create_group('/Nile')
+    nile.create_dataset('walkingHand', data=pd.read_csv('handWalkingNile.csv'), compression='gzip', compression_opts=7)
+    nile.create_dataset('walkingPant', data=pd.read_csv('pantWalkingNile.csv'), compression='gzip', compression_opts=7)
+    nile.create_dataset('walkingJacket', data=pd.read_csv('CoatWalkingNile.csv'), compression='gzip', compression_opts=7)
+    nile.create_dataset('jumpingPant', data=pd.read_csv('CharJumpingFrontPocket.csv'), compression='gzip', compression_opts=7)
+    nile.create_dataset('jumpingHand', data=pd.read_csv('CharJumpingHand.csv'), compression='gzip', compression_opts=7)
+    nile.create_dataset('jumpingJacket', data=pd.read_csv('CoatJumpingNile.csv'), compression='gzip', compression_opts=7)
+
+    dan = hdf.create_group('/Dan')
+    dan.create_dataset('walkingHand', data=pd.read_csv('CharWalkingHand.csv'), compression='gzip', compression_opts=7)
+    dan.create_dataset('walkingPant', data=pd.read_csv('CharWalkingFrontPocket.csv'), compression='gzip', compression_opts=7)
+    dan.create_dataset('walkingJacket', data=pd.read_csv('CharWalkingJacketPocket.csv'), compression='gzip', compression_opts=7)
+    dan.create_dataset('jumpingPant', data=pd.read_csv('CharJumpingFrontPocket.csv'), compression='gzip', compression_opts=7)
+    dan.create_dataset('jumpingHand', data=pd.read_csv('CharJumpingHand.csv'), compression='gzip', compression_opts=7)
