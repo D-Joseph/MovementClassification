0548ada8ed0484d2d3f3f2503c01cd87d29f5150
19ndc2 <73756295+19ndc2@users.noreply.github.com>
2023-04-11 00:07:55 -0400

    Added predict button the the GUI, file_path of output file now a string

--- gui.py
+++ gui.py
@@ -1,16 +1,23 @@
 import tkinter as tk
 from tkinter import ttk, filedialog
+import os
 
+window = tk.Tk()
 def saveFile():
     files = [('CSV file', '*.csv')]
     file_path = filedialog.asksaveasfile(filetypes=files, defaultextension='.csv')
-    print(file_path)
+
+    print(file_path.name)
+
+save_button = tk.Button(window, text='Save', command=saveFile)
+save_dialog = tk.Label(text="\nPredictions made, select location to save output file", font=('Helvetica', 16))
+
 
 def processResults():
-    save_dialog = tk.Label(text="\nPredictions made, select location to save output file", font=('Helvetica', 16))
     save_dialog.pack()
-    save_button = tk.Button(window, text='save', command=saveFile)
     save_button.pack()
+predict_button = tk.Button(window, text='Predict', command=processResults)
+predict_dialog = tk.Label(text="\nInput file uploaded", font=('Helvetica', 16))
 
 def searchFile():
     file_path = filedialog.askopenfilename(
@@ -18,12 +25,13 @@ def searchFile():
         title='Select a CSV file',
         filetypes=(('CSV files', '*.csv'),)
     )
-    predict_dialog = tk.Label(text="\nInput file uploaded", font=('Helvetica', 16))
-    predict_dialog.pack()
-    predict_button = tk.Button(window, text='predict', command=processResults)
-    predict_button.pack()
+    if file_path != '':
+        predict_button.pack_forget()
+        save_dialog.pack_forget()
+        save_button.pack_forget()
 
-window = tk.Tk()
+        predict_dialog.pack()
+        predict_button.pack()
 
 intro = tk.Label(text="Upload CSV file to determine if user is running or walking", font=('Helvetica', 16))
 intro.pack()
